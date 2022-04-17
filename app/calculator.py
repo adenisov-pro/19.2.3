dfc393f91694d2a47b457d5524e12f28cdb2331e
Andrey <adenisov.pro@gmail.com>
2022-04-17 10:25:21 +0300

    Add new repository

---
+++ app/calculator.py
@@ -0,0 +1,13 @@
+
+class Calculator: # создали класс Calculator
+   def multiply(self, x, y): # метод класса: умножение
+       return x * y
+
+   def division(self, x, y): # метод класса: деление
+       return x / y
+
+   def subtraction(self, x, y): #метод класса: вычитание
+       return x - y
+
+   def adding(self, x, y): # метод класса: сложение
+       return x + y
